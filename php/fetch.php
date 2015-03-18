979779d3b87c000abce43ed88d6cfd1cda5e16cd
Williams94 <r.b.williams94@gmail.com>
2015-03-17 20:21:08 -0400

    Small changes

--- php/fetch.php
+++ php/fetch.php
@@ -16,7 +16,7 @@ if($action=="gettemp"){
 
 
 
-    $data = R::getAll( 'SELECT * FROM `sensor1`  WHERE type=1  ORDER BY id DESC LIMIT 500' );
+    $data = R::getAll( 'SELECT * FROM `sensor1`  WHERE type=1  ORDER BY id DESC LIMIT 800' );
 
     echo json_encode($data);
 
